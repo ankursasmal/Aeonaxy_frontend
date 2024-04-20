73c6d3e420d851f54af9ceebe37a43b167e3c021
ANKUR SASMAL <ankursasmal@ANKURs-MacBook-Air.local>
2024-04-20 21:39:23 +0530

    2nd commit

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -26,7 +26,7 @@ function Blog() {
             <a className={`${style.textCenter} text-[3vw]  text-black`}> The Medium blog</a>
             <a className={`${style.textCenter1} text-black `} > Visit our company blog for the latest news, product updates, and tips and tricks.</a>
             <button className='text-green-400 border-green-400 border-[1px] sm:mt-[1vw]  mt-[4vw] rounded-full self-start mx-4 px-[2vw] py-[.5vw] mb-8'>Read our blog</button>
-            <div className={`${style.hiderule}font-bold bg-black w-[49vw] h-[.9px]`}></div>
+            <div className={`${style.hiderule}   font-bold bg-black w-[49vw] h-[.9px]`}></div>
 
 
           </div>
@@ -34,7 +34,7 @@ function Blog() {
             <a className={`${style.textCenter} text-[3vw]  text-black`}>Work at Medium</a>
             <a className={`${style.textCenter1} text-black  `} > Our team is home to engineers, journalists, artists,
               and creatives of all stripes.</a>
-            <button className='text-green-400 border-green-400 mx-7 border-[1px] sm:mt-[1vw] mt-[4vw]   rounded-full  self-start px-[2vw] py-[.5vw] sm:text-[.5vw] mb-8'>View open positions</button>
+            <button className='text-green-500 border-green-500 mx-7 border-[1px] sm:mt-[1vw] mt-[4vw]   rounded-full  self-start px-[2vw] py-[.5vw] sm:text-[.4vw] md:text-[1vw] lg:text-[1.4vw] mb-8'>View open positions</button>
 
 
           </div>
